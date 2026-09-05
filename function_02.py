3043280884049f65fd1c8a49e51f8b5f705ac223
avior-qc <avior.h@qodo.ai>
2026-09-05 21:52:18 +0300

    adjust logging in function_02.py

--- function_02.py
+++ function_02.py
@@ -4,5 +4,5 @@ logger = logging.getLogger(__name__)
 
 
 def function_02(value: int) -> int:
-    logger.info("function_02 received value=%s", value)
+    logger.warning("function_02 processing value=%s", value)
     return value + 2
