54dce8715f10288b0dffe6606a247f2ffcd4a661
avior-qc <avior.h@qodo.ai>
2026-09-05 21:53:18 +0300

    adjust logging in function_09.py

--- function_09.py
+++ function_09.py
@@ -4,5 +4,5 @@ logger = logging.getLogger(__name__)
 
 
 def function_09(value: int) -> int:
-    logger.info("function_09 received value=%s", value)
+    logger.info("function_09 executing with value=%s", value)
     return value + 9
